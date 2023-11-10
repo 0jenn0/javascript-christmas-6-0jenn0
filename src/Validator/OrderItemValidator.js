ff57757debb514dc34d22335d218717b647d101b
0jenn0 <jenn0.6n@gmail.com>
2023-11-10 17:52:38 +0900

    Fix: OrderItemValidator 메서드 수정
    input을 메뉴주문 1개가 아니라 여러개인걸로 수정

--- src/Validator/OrderItemValidator.js
+++ src/Validator/OrderItemValidator.js
@@ -3,17 +3,32 @@ import ERROR_MESSAGE from "../Error/message.js";
 import { MENU } from "../constants/menu.js";
 
 export default class OrderItemValidator {
-  #input;
+  #orderItems;
 
   constructor(input) {
-    this.#validateFormat(input);
-    this.#input = input;
+    const orderItems = input.split(",");
+    this.#orderItems = orderItems;
+  }
+
+  validateOrder() {
+    this.#validateFormat(this.#orderItems);
+    this.#validateIsInMenu(this.#orderItems);
   }
 
   #validateFormat() {
     const pattern = /^[가-힣]+-\d+$/;
 
-    if (pattern.test(this.#input)) {
+    if (this.#orderItems.some((item) => !pattern.test(item))) {
+      throw new AppError(ERROR_MESSAGE.invalid_format);
+    }
+  }
+
+  #validateIsInMenu(orderItems) {
+    const allMenu = Object.values(MENU).flat((item) => item.name);
+    const isAllinMenu = orderItems.every((item) =>
+      allMenu.includes(item.split("-")[0])
+    );
+    if (!isAllinMenu) {
       throw new AppError(ERROR_MESSAGE.invalid_format);
     }
   }
