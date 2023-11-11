d77cef3d254d1fc5aa89ca71ddeae22586c88a61
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 16:56:14 +0900

    Refactor: DayValidator 클래스를 static 클래스로 수정

--- src/Validator/DayValidator.js
+++ src/Validator/DayValidator.js
@@ -1,24 +1,19 @@
 import AppError from "../Error/AppError";
 
 export default class DayValidator {
-  #day;
-  constructor(day) {
-    this.#day = Number(day);
+  static validateDay(day) {
+    this.#validateType(day);
+    this.#validateRange(day);
   }
 
-  validateDay() {
-    this.#validateType();
-    this.#validateRange();
-  }
-
-  #validateType() {
-    if (isNaN(this.#day)) {
+  static #validateType(day) {
+    if (isNaN(day)) {
       throw new AppError("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
   }
 
-  #validateRange() {
-    if (this.#day > 31 || this.#day < 1) {
+  static #validateRange(day) {
+    if (Number(day) > 31 || Number(day) < 1) {
       throw new AppError("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
   }
