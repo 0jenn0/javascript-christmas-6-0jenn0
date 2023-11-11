8a1ac414f6146263442bd0840dcfd84079aacba9
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 17:20:22 +0900

    Test: ChristmasPromotionManager 클래스의 크리스마스 디데이 이벤트 관련 메서드 테스트 추가
    - 방문날짜가 1~25일이면 할인금액이 방문날짜의 따라 구해지는지
    - 1~25일이 아니면 0인지

--- __tests__/Model/ChristmasPromotionManagerTest.js
+++ __tests__/Model/ChristmasPromotionManagerTest.js
@@ -1,4 +1,8 @@
-import { OrderItem, ChristmasPromotionManager } from "../../src/Model/index.js";
+import {
+  OrderItem,
+  ChristmasPromotionManager,
+  Day,
+} from "../../src/Model/index.js";
 
 describe("ChristmasPromotionManager 클래스 테스트", () => {
   test("총주문 금액을 구해야한다.", () => {
@@ -6,8 +10,31 @@ describe("ChristmasPromotionManager 클래스 테스트", () => {
       new OrderItem("티본스테이크", "2"),
       new OrderItem("레드와인", "1"),
     ];
-    const christmasPromotionManager = new ChristmasPromotionManager(input);
+    const day = new Day("10");
+    const christmasPromotionManager = new ChristmasPromotionManager(input, day);
 
     expect(christmasPromotionManager.calculateAllOrderPrice()).toBe(170_000);
+  });
+
+  test("방문날짜가 1~25일이면 크리스마스 디데이 할인금액을 구해야한다.", () => {
+    const input = [
+      new OrderItem("티본스테이크", "2"),
+      new OrderItem("레드와인", "1"),
+    ];
+    const day = new Day("25");
+    const christmasPromotionManager = new ChristmasPromotionManager(input, day);
+
+    expect(christmasPromotionManager.calculateDdayDiscount()).toBe(3_400);
+  });
+
+  test("방문날짜가 1~25일이 아니면 크리스마스 디데이 할인금액 0원이어야한다.", () => {
+    const input = [
+      new OrderItem("티본스테이크", "2"),
+      new OrderItem("레드와인", "1"),
+    ];
+    const day = new Day("30");
+    const christmasPromotionManager = new ChristmasPromotionManager(input, day);
+
+    expect(christmasPromotionManager.calculateDdayDiscount()).toBe(0);
   });
 });
