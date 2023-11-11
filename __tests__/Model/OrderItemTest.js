679f6880636f109f11714bfca1dbe59ac429d712
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 18:33:55 +0900

    Test: OrderItem 클래스 메뉴 카테고리 구하는 메서드 테스트 추가

--- __tests__/Model/OrderItemTest.js
+++ __tests__/Model/OrderItemTest.js
@@ -5,4 +5,9 @@ describe("OrderItem 클래스 테스트", () => {
     const orderItem = new OrderItem("바비큐립", "2");
     expect(orderItem.calculateTotalPrice()).toBe(108_000);
   });
+
+  test("메뉴의 카테고리를 알 수 있어야한다.", () => {
+    const orderItem = new OrderItem("초코케이크", "2");
+    expect(orderItem.findMenuCategory()).toBe("dessert");
+  });
 });
