adadc429ee7d415b5dc1baeb90c0479a9c3ab6ec
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 19:55:37 +0900

    Test: Calendar 클래스 수정됨을 테스트 코드에 적용

--- __tests__/Model/CalendarTest.js
+++ __tests__/Model/CalendarTest.js
@@ -2,18 +2,22 @@ import { Calendar } from "../../src/Model";
 
 describe("Calendar 클래스 테스트", () => {
   test("금요일,토요일은 주말이다", () => {
-    const calendar = new Calendar(2023, 12);
+    const calendar1 = new Calendar(1);
+    const calendar2 = new Calendar(2);
+    const calendar3 = new Calendar(3);
 
-    expect(calendar.isWeekend(1)).toBe(true);
-    expect(calendar.isWeekend(2)).toBe(true);
-    expect(calendar.isWeekend(3)).toBe(false);
+    expect(calendar1.isWeekend()).toBe(true);
+    expect(calendar2.isWeekend()).toBe(true);
+    expect(calendar3.isWeekend()).toBe(false);
   });
 
   test("12월의 일요일과 25일은 특별 할인이 적용되는 날이다.", () => {
-    const calendar = new Calendar(2023, 12);
+    const calendar1 = new Calendar(3);
+    const calendar2 = new Calendar(25);
+    const calendar3 = new Calendar(11);
 
-    expect(calendar.isSpecailDay(3)).toBe(true);
-    expect(calendar.isSpecailDay(25)).toBe(true);
-    expect(calendar.isSpecailDay(11)).toBe(false);
+    expect(calendar1.isSpecailDay(3)).toBe(true);
+    expect(calendar2.isSpecailDay(25)).toBe(true);
+    expect(calendar3.isSpecailDay(11)).toBe(false);
   });
 });
