16349c176b2b5c3e0fee3c87fd0dfa0dd8de215f
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 14:25:24 +0900

    Test: OrderItemValidator 중복 메뉴 검증 메서드 테스트 추가

--- __tests__/Validator/OrderItemValidatorTest.js
+++ __tests__/Validator/OrderItemValidatorTest.js
@@ -35,10 +35,16 @@ describe("OrderItemValidator 클래스 테스트", () => {
   });
 
   test("메뉴 주문은 최대 20개 까지만 가능하다. - 엣지케이스", () => {
-    const input = "양송이수프-10,티본스테이크-10";
+    const input = "양송이수프-10,제로콜라-10";
     const orderItemsValidator = new OrderItemValidator(input);
     expect(() => orderItemsValidator.validateOrder()).not.toThrow(
       AppError.PREFIX
     );
+  });
+
+  test("메뉴 주문시 메뉴 중복 불가하다.", () => {
+    const input = "제로콜라-10,제로콜라-5";
+    const orderItemsValidator = new OrderItemValidator(input);
+    expect(() => orderItemsValidator.validateOrder()).toThrow(AppError.PREFIX);
   });
 });
