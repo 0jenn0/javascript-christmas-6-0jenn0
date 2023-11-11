173740597137d79c3c60ad58858887570844b0ef
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 18:07:07 +0900

    Test: Calendar 클래스에서 스페셜 할인이 적용될수있는지 구하는 메서드 테스트 추가

--- __tests__/Model/CalendarTest.js
+++ __tests__/Model/CalendarTest.js
@@ -8,4 +8,12 @@ describe("Calendar 클래스 테스트", () => {
     expect(calendar.isWeekend(2)).toBe(true);
     expect(calendar.isWeekend(3)).toBe(false);
   });
+
+  test("12월의 일요일과 25일은 특별 할인이 적용되는 날이다.", () => {
+    const calendar = new Calendar(2023, 12);
+
+    expect(calendar.isSpecailDay(3)).toBe(true);
+    expect(calendar.isSpecailDay(25)).toBe(true);
+    expect(calendar.isSpecailDay(11)).toBe(false);
+  });
 });
