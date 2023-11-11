9a4f0bcac7ce8464e5d9b60852b002543383f30f
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 16:58:01 +0900

    Test: DayValidator 클래스에 static 클래스로 수정됨을 적용

--- __tests__/Validator/DayValidatorTest.js
+++ __tests__/Validator/DayValidatorTest.js
@@ -2,22 +2,27 @@ import AppError from "../../src/Error/AppError.js";
 import DayValidator from "../../src/Validator/DayValidator.js";
 
 describe("DayValidator 클래스 테스트", () => {
+  test("날자는 1이상 31이하의 숫자여야한다. - 정상 케이스", () => {
+    const input = "2";
+
+    expect(() => DayValidator.validateDay(input)).not.toThrow(AppError.PREFIX);
+  });
+
   test("날자는 1이상 31이하의 숫자여야한다.", () => {
     const input = "d";
 
-    const dayValidator = new DayValidator(input);
-
-    expect(() => dayValidator.validateDay()).toThrow(AppError.PREFIX);
+    expect(() => DayValidator.validateDay(input)).toThrow(AppError.PREFIX);
   });
 
   test("날자는 1이상 31이하의 숫자여야한다. - 엣지케이스", () => {
     const inputMin = "1";
     const inputMax = "31";
 
-    const minDayValidator = new DayValidator(inputMin);
-    const maxDayValidator = new DayValidator(inputMax);
-
-    expect(() => minDayValidator.validateDay()).not.toThrow(AppError.PREFIX);
-    expect(() => maxDayValidator.validateDay()).not.toThrow(AppError.PREFIX);
+    expect(() => DayValidator.validateDay(inputMin)).not.toThrow(
+      AppError.PREFIX
+    );
+    expect(() => DayValidator.validateDay(inputMax)).not.toThrow(
+      AppError.PREFIX
+    );
   });
 });
