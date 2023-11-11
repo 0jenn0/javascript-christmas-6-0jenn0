15fab8f22d35eabe49a7c06b2abcd96a90636a5f
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 16:00:58 +0900

    Test: OrderItemValidator 클래스 테스트 코드 input 형식을 문자열에서 리스트로 수정
    InputView에서 ,로 구분하여 리스트로 받을거기때문에 이렇게 수정함

--- __tests__/Validator/OrderItemValidatorTest.js
+++ __tests__/Validator/OrderItemValidatorTest.js
@@ -3,54 +3,65 @@ import OrderItemValidator from "../../src/Validator/OrderItemValidator";
 
 describe("OrderItemValidator 클래스 테스트", () => {
   test("정상 케이스", () => {
-    const input = "양송이수프-1";
-    const orderItemsValidator = new OrderItemValidator(input);
+    const input = ["양송이수프-1"];
 
-    expect(() => orderItemsValidator.validateOrder()).not.toThrow(
+    expect(() => OrderItemValidator.validateOrder(input)).not.toThrow(
       AppError.PREFIX
     );
   });
   test("메뉴판에있는 메뉴만 주문할 수 있다.", () => {
-    const input = "짜파구리-2,스파게티-1";
-    const orderItemsValidator = new OrderItemValidator(input);
-    expect(() => orderItemsValidator.validateOrder()).toThrow(AppError.PREFIX);
+    const input = ["짜파구리-2", "스파게티-1"];
+
+    expect(() => OrderItemValidator.validateOrder(input)).toThrow(
+      AppError.PREFIX
+    );
   });
 
   test("메뉴 주문시 형식을 지켜야한다.", () => {
-    const input = "양송이수프 2개,타파스 1개";
-    const orderItemsValidator = new OrderItemValidator(input);
-    expect(() => orderItemsValidator.validateOrder()).toThrow(AppError.PREFIX);
+    const input = ["양송이수프 2개", "타파스 1개"];
+
+    expect(() => OrderItemValidator.validateOrder(input)).toThrow(
+      AppError.PREFIX
+    );
   });
 
   test("메뉴 주문은 최소 1개부터 가능하다.", () => {
-    const input = "양송이수프-0";
-    const orderItemsValidator = new OrderItemValidator(input);
-    expect(() => orderItemsValidator.validateOrder()).toThrow(AppError.PREFIX);
+    const input = ["양송이수프-0"];
+
+    expect(() => OrderItemValidator.validateOrder(input)).toThrow(
+      AppError.PREFIX
+    );
   });
 
   test("메뉴 주문은 최대 20개 까지만 가능하다.", () => {
-    const input = "양송이수프-10,티본스테이크-10,제로콜라-10";
-    const orderItemsValidator = new OrderItemValidator(input);
-    expect(() => orderItemsValidator.validateOrder()).toThrow(AppError.PREFIX);
+    const input = ["양송이수프-10", "티본스테이크-10", "제로콜라-10"];
+
+    expect(() => OrderItemValidator.validateOrder(input)).toThrow(
+      AppError.PREFIX
+    );
   });
 
   test("메뉴 주문은 최대 20개 까지만 가능하다. - 엣지케이스", () => {
-    const input = "양송이수프-10,제로콜라-10";
-    const orderItemsValidator = new OrderItemValidator(input);
-    expect(() => orderItemsValidator.validateOrder()).not.toThrow(
+    const input = ["양송이수프-10", "제로콜라-10"];
+
+    expect(() => OrderItemValidator.validateOrder(input)).not.toThrow(
       AppError.PREFIX
     );
   });
 
   test("메뉴 주문시 메뉴 중복 불가하다.", () => {
-    const input = "제로콜라-10,제로콜라-5";
-    const orderItemsValidator = new OrderItemValidator(input);
-    expect(() => orderItemsValidator.validateOrder()).toThrow(AppError.PREFIX);
+    const input = ["제로콜라-10", "제로콜라-5"];
+
+    expect(() => OrderItemValidator.validateOrder(input)).toThrow(
+      AppError.PREFIX
+    );
   });
 
   test("음료만 주문 불가하다.", () => {
-    const input = "제로콜라-10,레드와인-5";
-    const orderItemsValidator = new OrderItemValidator(input);
-    expect(() => orderItemsValidator.validateOrder()).toThrow(AppError.PREFIX);
+    const input = ["제로콜라-10", "레드와인-5"];
+
+    expect(() => OrderItemValidator.validateOrder(input)).toThrow(
+      AppError.PREFIX
+    );
   });
 });
