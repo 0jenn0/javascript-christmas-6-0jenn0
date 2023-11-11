3ffaca9e6dc6c87de32cb87bcc37508cb9e4f593
0jenn0 <jenn0.6n@gmail.com>
2023-11-11 20:58:28 +0900

    Feat: ChristmasPromotionManager 클래스에 총할인금액 계산,총금액이 1만원 이상일때 이벤트실행 메서드 추가

--- src/Model/ChristmasPromotionManager.js
+++ src/Model/ChristmasPromotionManager.js
@@ -13,6 +13,25 @@ export default class ChristmasPromotionManager {
     this.#day = Number(day);
   }
 
+  calculatePriceAfterPromotion() {
+    const priceBeforePromotion = this.calculateAllOrderPrice();
+
+    if (priceBeforePromotion >= 10_000) {
+      const totalDiscount = this.calculateTotalDiscount();
+      return priceBeforePromotion - totalDiscount;
+    }
+    return priceBeforePromotion;
+  }
+
+  calculateTotalDiscount() {
+    let totalDiscount = 0;
+    totalDiscount += this.calculateDdayDiscount();
+    totalDiscount += this.calculateWeekendDiscount().discount;
+    totalDiscount += this.calculateSpecialDiscount();
+    totalDiscount += this.calculateGiftMenu();
+    return totalDiscount;
+  }
+
   calculateAllOrderPrice() {
     const allOrderPrice = this.#orderItemList.reduce((accPrice, orderItem) => {
       accPrice += orderItem.calculateTotalPrice();
