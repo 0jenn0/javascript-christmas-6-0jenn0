b27bc08c55c0ef4821a277ab5f396d00371d1484
0jenn0 <jenn0.6n@gmail.com>
2023-11-12 16:19:20 +0900

    Feat: OutputView 인사 출력추가

---
+++ src/View/OutputView.js
@@ -0,0 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MENU_CATEGORIES } from "../constants/menu.js";
+
+const OutputView = {
+  printHello() {
+    Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+  },
+};
+
+export default OutputView;
