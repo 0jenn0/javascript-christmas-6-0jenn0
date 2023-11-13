1fbc27b62c7ff3f87b0da88ce67e1867a606c5c8
0jenn0 <jenn0.6n@gmail.com>
2023-11-13 23:05:24 +0900

    Feat: 주문 입력받을 때 메시지에 주문 메뉴에 관한 이벤트 주의사항 추가

--- src/constants/viewMessages.js
+++ src/constants/viewMessages.js
@@ -26,14 +26,14 @@ export const MESSAGES = Object.freeze({
   WELCOME: "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
   EVENT_PREVIEW: (day) =>
     `12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
-  EVENT_INTRO: `[이벤트 안내] 총주문 금액 ${ORDER_CONSTRAINTS.MIN_ORDER_AMOUNT}원 이상부터 이벤트가 적용됩니다.`,
+  EVENT_INTRO: `- 총주문 금액 ${ORDER_CONSTRAINTS.MIN_ORDER_AMOUNT}원 이상부터 이벤트가 적용됩니다.`,
   NEW_YEAR_EVENT_NOTICE:
     "(배지에 따라 2024 새해 이벤트 참여 시, 각각 다른 새해 선물을 증정할 예정입니다.)",
   NONE: "없음",
   READ_DATE:
     "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)",
-  READ_MENU:
-    "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)",
+  READ_MENU: `주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)
+- 메뉴는 한 번에 최대 ${ORDER_CONSTRAINTS.MAX_MENU_ITEMS}개까지만 주문할 수 있습니다.
+- 음료만 주문 시, 주문할 수 없습니다.`,
   FORMAT_MENU_ITEM: (menuName, quantity) => `${menuName} ${quantity}개`,
-  NONE: "없음",
 });
